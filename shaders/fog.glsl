34ba9770de7ce0a113ee15b9bcf858e737bc795c
ACupOfChicory <acupofchicory@gmail.com>
2026-08-29 13:42:03 +0500

    fog fix + sync
    the fog and rain interaction is broken

--- shaders/fog.glsl
+++ shaders/fog.glsl
@@ -1,1 +1,49 @@
+uniform vec3 fogColor;
+uniform float blindness;
+uniform int isEyeInWater;
+uniform float viewHeight;
+uniform float viewWidth;
+uniform float eyeAltitude;
+uniform mat4 gbufferProjectionInverse;
 
+// UNDO SOMETHING IDK
+vec3 screenToView(vec3 screenPos) {
+    vec4 ndcPos = vec4(screenPos, 1.0) * 2.0 - 1.0;
+    vec4 tmp = gbufferProjectionInverse * ndcPos;
+    return tmp.xyz / tmp.w;
+}
+
+
+
+float FoggyFog(vec2 texcoord, float depth, out vec3 fogColorOut){
+    vec3 depthcoord = vec3(texcoord, depth);
+    vec3 bereal = screenToView(depthcoord);
+    float realdistance = length(bereal);
+    float PhileasFogg;
+    
+    if(isEyeInWater == 0){
+        PhileasFogg = smoothstep(fogStart, fogEnd, realdistance);
+        fogColorOut = mix(fogColor, vec3(0.0), smoothstep(caveFogHigh, caveFogLow, eyeAltitude));
+        
+    }
+    if(isEyeInWater == 1){
+        PhileasFogg = smoothstep(10.0, 20.0, realdistance);
+        fogColorOut = vec3(0.1, 0.1, 0.5);
+        
+    }
+    if(isEyeInWater == 2){
+        PhileasFogg = smoothstep(4.0, 10.0, realdistance);
+        fogColorOut = vec3(0.6, 0.1, 0.0);
+        
+    }
+    if(isEyeInWater == 3){
+        PhileasFogg = smoothstep(2.0, 5.0, realdistance);
+        fogColorOut = vec3(0.710, 0.918, 0.918);
+        
+        
+    }
+    PhileasFogg = mix(PhileasFogg, 1.0, blindness);
+    return PhileasFogg;
+    
+    
+}
--- shaders/gbuffers_weather.fsh
+++ shaders/gbuffers_weather.fsh
@@ -5,7 +5,6 @@
 uniform sampler2D lightmap;
 uniform sampler2D gtexture;
 
-
 in vec2 lmcoord;
 in vec2 texcoord;
 in vec4 glcolor;
@@ -13,10 +12,47 @@ in vec4 glcolor;
 /* RENDERTARGETS: 0 */
 layout(location = 0) out vec4 color;
 
+#include "fog.glsl"
+
 //light function
 #include "light.glsl"
 
 void main() {
 	color = texture(gtexture, texcoord) * glcolor;
-    lightFunction(lightmap, lmcoord);
+    //color = lightFunction(lightmap, lmcoord);
+    //color *= texture(lightmap, lmcoord);
+    vec3 fogTint;
+    float fogAmount = FoggyFog(gl_FragCoord.xy / vec2(viewWidth, viewHeight), gl_FragCoord.z, fogTint);
+    color.rgb = mix(color.rgb, fogTint, fogAmount);
 }
+
+
+
+
+Русский язык - 48
+
+Литература - 48
+
+Алгебра - 98
+
+Геометрия - 98
+
+Иностранный язык - 48
+
+Физика - 98
+
+Химия - Тетрадь общая на 48 листов, Тетрадь на 12 листов. в клетку. Калькулятор обычный
+
+Биология - 48
+
+История - 48
+
+География - 48
+
+(возможно) Информатика
+
+(возможно) Вероятность и статистика
+
+прочее:
+Карандаши, ручки обычные шариковые.
+пенал, точилка, ластик
